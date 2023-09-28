c8a200ee0bcc26f5a4503ad93d8bc62529aef2e7
craymahdee142 <craymahdaniel@gamil.com>
2023-09-28 00:09:11 +0000

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,6 +2,6 @@
 
 exec {'install':
     provider => 'shell',
-    command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; echo "Hello world!" | sudo tee /var/www/html/index.html ; sudo sed -i "s/server_name_;/server_name_;\n\trewrite ^ \/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; /etc/nginx/sites-available/default ; sudo service nginx start',
+    command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello world!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name_;/server_name_;\n\trewrite ^ \/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; /etc/nginx/sites-available/default ; sudo service nginx start',
 } 
 
