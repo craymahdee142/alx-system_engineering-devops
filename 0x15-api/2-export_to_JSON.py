8a5b95045ab04c80ea19b4e2cb2dc5702ee7a825
craymahdee142 <craymahdaniel@gamil.com>
2023-10-24 01:47:00 +0000

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,30 +1,38 @@
-
 #!/usr/bin/python3
 """script that export data to JSON file"""
 
+
+import json
 import requests as req
 import sys
-import json
 
-if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com/users"
-    url = base_url + "/" + employeeId
 
-    res = req.get(url)
-    username = res.json().get('username')
+# Define base URL
+base_url = "https://jsonplaceholder.typicode.com/"
 
-    todo_url = url + "/todos"
-    res = req.get(todo_url)
-    tasks = res.json()
 
-    dictionary = {employeeId: []}
-    for task in tasks:
-        dictionary[employeeId].append({
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": username
-        })
+# Define a function to get user data
+def get_user_data(user_id):
+    user = req.get(f"{base_url}users/{user_id}").json()
+    username = user.get("username")
+    todos = req.get(f"{base_url}todos", params={"userId": user_id}).json()
+    return user_id, username, todos
 
-    with open('{}.json'.format(employeeId), 'w') as filename:
-        json.dump(dictionary, filename)
+
+# Define a function to export data to JSON
+def export_to_json(filename, data):
+    with open(f"{filename}.json", "w") as jsonfile:
+        json.dump({data[0]: [{
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": data[1]
+        } for task in data[2]]}, jsonfile)
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+
+    # Get user data
+    user_data = get_user_data(user_id)
+    # Export data to JSON
+    export_to_json(user_id, user_data)
