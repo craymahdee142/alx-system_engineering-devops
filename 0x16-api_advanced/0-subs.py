4608a59e664cb4a7801608fa56f1862c9c67e14e
craymahdee142 <craymahdaniel@gamil.com>
2023-11-10 02:00:14 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Function to query number of subscribers"""
-import requests as req
+import requests
 
 
 def number_of_subscribers(subreddit):
     """return total number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    res = req.get(url, headers=headers, allow_redirects=False)
+    res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 404:
         return (0)
     results = res.json().get("data")
